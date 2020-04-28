3fed92a1085324c2efcbb50db4f98df219cccb15
Boyu <rider945@outlook.com>
2020-04-28 14:18:32 +0800

    feature/task_2 done

--- index.js
+++ index.js
@@ -6,4 +6,30 @@ document.addEventListener('DOMContentLoaded',function(){
     document.querySelector('#navbar-menu').classList.toggle("is-active")
   })
 
+  // feature/tesk 2
+  let form = document.querySelector('#search-job')
+  
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+    let formData = 
+      {
+        "description": form.elements["description"].value,
+        "location": form.elements["location"].value,
+        "full_time": form.elements["full_time"].checked
+      }
+
+    fetch(getUri(formData))
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(myJson);
+    });
+  })
+
+  function getUri(data){
+    return `https://still-spire-37210.herokuapp.com/positions.json?description=${data.description}&location=${data.location}&full_time=${data.full_time}`
+  }
+
+
 })
